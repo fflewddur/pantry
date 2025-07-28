6b1357807498552134e018c988f20b1dd8d0e963
Todd Kulesza <todd@dropline.net>
2025-07-27 20:42:18 -0700

    setup project infrastructure

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -9,7 +9,12 @@ import (
 func main() {
 	log.Println("The server is starting...")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		_, err := fmt.Fprintf(w, "Hello, World!")
+		if err != nil {
+			log.Printf("Error writing response: %v", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
