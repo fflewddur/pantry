9f0f9ebae7bf1d8a7ee77fa73ae3780d5d2fea75
Todd Kulesza <todd@dropline.net>
2025-07-28 17:38:18 -0700

    setup infra for downloading each module

--- cmd/scanner/scanner.go
+++ cmd/scanner/scanner.go
@@ -13,13 +13,18 @@ import (
 
 func main() {
 	log.Println("Starting the scanner...")
+	modules := make(map[string]*ModuleEntry)
 	gotResults := true
-	since := time.Now().AddDate(0, 0, -1) // Set the time to 7 days ago
-	url := "https://index.golang.org/index"
+	maxModules := 100                     // Limit the number of modules to fetch
+	since := time.Now().AddDate(0, 0, -1) // Set the time to 1 day ago
+	urlBase := "https://index.golang.org/index"
 	c := http.Client{}
-	for gotResults {
-		log.Printf("Fetching modules since %s", since.Format(time.RFC3339))
-		resp, err := c.Get(fmt.Sprintf("%s?since=%s", url, since.Format(time.RFC3339)))
+
+	for gotResults && len(modules) < maxModules {
+		log.Printf("Fetching modules since %s (%d of %d)", since.Format(time.RFC3339), len(modules), maxModules)
+
+		url := fmt.Sprintf("%s?since=%s&limit=%d", urlBase, since.Format(time.RFC3339), maxModules-len(modules))
+		resp, err := c.Get(url)
 		if err != nil {
 			log.Fatalf("Failed to make request: %v", err)
 		}
@@ -40,13 +45,15 @@ func main() {
 			}
 			count++
 			var entry ModuleEntry
-			// log.Printf("line: %s", line)
 			if err := json.Unmarshal(line, &entry); err != nil {
 				log.Printf("Error unmarshaling response for line %s: %v", line, err)
 				continue
 			}
+			_, found := modules[entry.Path]
+			if !found {
+				modules[entry.Path] = &entry
+			}
 			since = entry.Timestamp
-			fmt.Printf("Module: %s, Version: %s, Timestamp: %s\n", entry.Path, entry.Version, entry.Timestamp)
 		}
 		if count <= 1 {
 			gotResults = false // No more data to process
@@ -54,11 +61,81 @@ func main() {
 			continue
 		}
 	}
-	// log.Printf("Received data: %s", data)
+	log.Printf("Processed %d modules since %s", len(modules), since.Format(time.RFC3339))
 
-	log.Println("Scanner completed")
+	latest := fetchLatest(modules)
+	downloadModules(latest)
 }
 
+func fetchLatest(modules map[string]*ModuleEntry) map[string]*Info {
+	// https://proxy.golang.org/cached-only/github.com/fflewddur/bsky/@latest
+	count := 0
+	latest := make(map[string]*Info, len(modules))
+	for _, mod := range modules {
+		count++
+		log.Printf("Fetching latest version for module: %s (%d of %d)", mod.Path, count, len(modules))
+		url := fmt.Sprintf("https://proxy.golang.org/cached-only/%s/@latest", mod.Path)
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Printf("Failed to fetch latest version for %s: %v", mod.Path, err)
+			continue
+		}
+		defer func() {
+			err = errors.Join(err, resp.Body.Close())
+		}()
+		if resp.StatusCode != http.StatusOK {
+			log.Printf("Unexpected status code for %s: %d", mod.Path, resp.StatusCode)
+			continue
+		}
+		data, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Printf("Failed to read response body for %s: %v", mod.Path, err)
+			continue
+		}
+		var info Info
+		if err := json.Unmarshal(data, &info); err != nil {
+			log.Printf("Error unmarshaling response for %s: %v", mod.Path, err)
+			continue
+		}
+		latest[mod.Path] = &info
+		// log.Printf("Latest version for module %s is %s at %s", mod.Path, info.Version, info.Time.Format(time.RFC3339))
+	}
+	return latest
+}
+
+func downloadModules(latest map[string]*Info) {
+	count := 0
+	for path, info := range latest {
+		count++
+		log.Printf("Downloading module %s (%d of %d)", path, count, len(latest))
+		url := fmt.Sprintf("https://proxy.golang.org/cached-only/%s/@v/%s.zip", path, info.Version)
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Printf("Failed to download module %s: %v", path, err)
+			continue
+		}
+		defer func() {
+			err = errors.Join(err, resp.Body.Close())
+		}()
+		if resp.StatusCode != http.StatusOK {
+			log.Printf("Unexpected status code for %s: %d", path, resp.StatusCode)
+			continue
+		}
+		_, err = io.ReadAll(resp.Body)
+		if err != nil {
+			log.Printf("Failed to read response body for %s: %v", path, err)
+			continue
+		}
+		// TODO: parse the zip file and extract the contents
+	}
+}
+
+type Info struct {
+	Version string    // version string
+	Time    time.Time // commit time
+}
+
+// ModuleEntry represents a single module entry in the Go index.
 type ModuleEntry struct {
 	Path      string    `json:"Path"`
 	Version   string    `json:"Version"`
