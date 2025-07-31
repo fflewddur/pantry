409f25166533c5fa03c0822654f60f9560acd4f4
Todd Kulesza <todd@dropline.net>
2025-07-30 19:56:08 -0700

    setup basic server infrastructure

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,21 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 func main() {
 	log.Println("The server is starting...")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintf(w, "Hello, World!")
-		if err != nil {
-			log.Printf("Error writing response: %v", err)
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-	})
-
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/search", searchHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request for search page")
+	tmpl, err := template.New("search.html").ParseFiles("templates/search.html")
+	if err != nil {
+		log.Printf("Error parsing template: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request for search results")
+	q := r.URL.Query().Get("q")
+	searchResults := &SearchResults{
+		Query: q,
+	}
+
+	// TODO: Implement actual search logic here
+
+	tmpl, err := template.New("results.html").ParseFiles("templates/results.html")
+	if err != nil {
+		log.Printf("Error parsing template: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, searchResults)
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+type SearchResults struct {
+	Query string
 }
