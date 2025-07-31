88822907a4e43756d11d69218d0f9b2eb98e3042
Todd Kulesza <todd@dropline.net>
2025-07-30 18:52:41 -0700

    download module readme files and store them

--- cmd/scanner/scanner.go
+++ cmd/scanner/scanner.go
@@ -1,23 +1,53 @@
 package main
 
 import (
+	"archive/zip"
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
 	"time"
+
+	_ "modernc.org/sqlite"
 )
+
+type Module struct {
+	Id      int64
+	Path    string
+	Version string
+	Readme  string
+	Time    time.Time
+}
 
 func main() {
 	log.Println("Starting the scanner...")
 	modules := make(map[string]*ModuleEntry)
 	gotResults := true
-	maxModules := 100                     // Limit the number of modules to fetch
+	maxModules := 10                      // Limit the number of modules to fetch
 	since := time.Now().AddDate(0, 0, -1) // Set the time to 1 day ago
 	urlBase := "https://index.golang.org/index"
+	// Initialize the database connection
+	var db *sql.DB
+	db, err := sql.Open("sqlite", "file:./data/mods.db?mode=rw&_journal_mode=WAL&_busy_timeout=5000")
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS mods (
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		path TEXT NOT NULL UNIQUE,
+		version TEXT NOT NULL,
+		readme TEXT,
+		time DATETIME NOT NULL
+	);`)
+	if err != nil {
+		log.Fatalf("Failed to create table: %v", err)
+	}
 	c := http.Client{}
 
 	for gotResults && len(modules) < maxModules {
@@ -61,10 +91,9 @@ func main() {
 			continue
 		}
 	}
-	log.Printf("Processed %d modules since %s", len(modules), since.Format(time.RFC3339))
-
 	latest := fetchLatest(modules)
-	downloadModules(latest)
+	downloadModules(latest, db)
+	log.Printf("Processed %d modules since %s", len(modules), since.Format(time.RFC3339))
 }
 
 func fetchLatest(modules map[string]*ModuleEntry) map[string]*Info {
@@ -103,7 +132,7 @@ func fetchLatest(modules map[string]*ModuleEntry) map[string]*Info {
 	return latest
 }
 
-func downloadModules(latest map[string]*Info) {
+func downloadModules(latest map[string]*Info, db *sql.DB) {
 	count := 0
 	for path, info := range latest {
 		count++
@@ -121,13 +150,68 @@ func downloadModules(latest map[string]*Info) {
 			log.Printf("Unexpected status code for %s: %d", path, resp.StatusCode)
 			continue
 		}
-		_, err = io.ReadAll(resp.Body)
+		data, err := io.ReadAll(resp.Body)
 		if err != nil {
 			log.Printf("Failed to read response body for %s: %v", path, err)
 			continue
 		}
-		// TODO: parse the zip file and extract the contents
+		mod := &Module{
+			Path:    path,
+			Version: info.Version,
+			Time:    info.Time,
+		}
+		txt, err := extractContent(data)
+		if err != nil {
+			log.Printf("Failed to extract content for %s: %v", path, err)
+			continue
+		}
+		mod.Readme = txt
+		db.Exec(`INSERT OR REPLACE INTO mods (path, version, readme, time) VALUES (?, ?, ?, ?)`,
+			mod.Path, mod.Version, mod.Readme, mod.Time)
 	}
+}
+
+func extractContent(data []byte) (string, error) {
+	reader, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		return "", fmt.Errorf("failed to create zip reader: %w", err)
+	}
+	// Create a regexp to match README files
+	readmeRegex := regexp.MustCompile(`(?i)readme(\.md|\.txt)?$`)
+	foundReadme := false
+	readmeContents := strings.Builder{}
+	for _, file := range reader.File {
+		// Just look at the README files
+		if !readmeRegex.MatchString(file.Name) {
+			continue
+		}
+		foundReadme = true
+		log.Printf("Extracting file: %s (compressed size: %d, uncompressed size: %d)", file.Name, file.CompressedSize64, file.UncompressedSize64)
+		rc, err := file.Open()
+		if err != nil {
+			log.Printf("Failed to open file %s in zip: %v", file.Name, err)
+			continue
+		}
+		defer func() {
+			err = errors.Join(err, rc.Close())
+		}()
+		content, err := io.ReadAll(rc)
+		if err != nil {
+			log.Printf("Failed to read content of file %s: %v", file.Name, err)
+			continue
+		}
+		readmeContents.Write(content)
+		readmeContents.WriteByte('\n') // Add a newline for separation
+		// log.Printf("Content of %s: %s\n", file.Name, content) // For demonstration purposes
+	}
+	if !foundReadme {
+		log.Printf("No README files found in the zip archive:")
+		for _, file := range reader.File {
+			log.Printf("  - %s (compressed size: %d, uncompressed size: %d)", file.Name, file.CompressedSize64, file.UncompressedSize64)
+		}
+	}
+	log.Println("Finished extracting content from zip file.")
+	return readmeContents.String(), nil
 }
 
 type Info struct {
