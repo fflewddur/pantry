d7876eb9914e9757ac2e25a2fb3f68c935f6b073
Todd Kulesza <todd@dropline.net>
2025-07-31 21:48:56 -0700

    setup module details page

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # pantry
+
 Early thoughts on navigating a package ecosystem
+
+## Components
+
+### Scanner
+
+The scanner ingests Go modules (as seen by index.golang.org) into a database.
+It extracts documentation for each package in the module, and stores this so
+that it can be used by the search engine.
+
+Start the scanner with the following command:
+
+```shell
+go run cmd/server/server.go
+```
+
+### Server
+
+The web server provides a searchable interface for the database of packages
+found by the scanner.
+
+Start the web server with the following command:
+
+```shell
+go run cmd/scanner/scanner.go
+```
+
+### Search engine
+
+Todo. We'll use Sphinx Search to search and rank packages.
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,19 +1,52 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"log"
 	"net/http"
+	"time"
+
+	_ "modernc.org/sqlite"
 )
 
 func main() {
 	log.Println("The server is starting...")
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/search", searchHandler)
+	server := NewServer()
+	server.Start()
+	log.Println("Server started on :8080")
+}
+
+type Server struct {
+	db *sql.DB
+}
+
+func NewServer() *Server {
+	db, err := sql.Open("sqlite", "file:./data/mods.db?mode=rw&_journal_mode=WAL&_busy_timeout=5000")
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS mods (
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		path TEXT NOT NULL UNIQUE,
+		version TEXT NOT NULL,
+		readme TEXT,
+		time DATETIME NOT NULL
+	);`)
+	if err != nil {
+		log.Fatalf("Failed to create default database: %v", err)
+	}
+	return &Server{db: db}
+}
+
+func (s *Server) Start() {
+	http.HandleFunc("/", s.rootHandler)
+	http.HandleFunc("/search", s.searchHandler)
+	http.HandleFunc("/mod/", s.modHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) rootHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Received request for search page")
 	tmpl, err := template.New("search.html").ParseFiles("templates/search.html")
 	if err != nil {
@@ -29,7 +62,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) searchHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Received request for search results")
 	q := r.URL.Query().Get("q")
 	searchResults := &SearchResults{
@@ -54,4 +87,49 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 
 type SearchResults struct {
 	Query string
+}
+
+func (s *Server) modHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request for mod page")
+	path := r.URL.Path[len("/mod/"):] // Extract the path after /mod/
+	var version string
+	var readme sql.NullString
+	var t time.Time
+	err := s.db.QueryRow("SELECT version, time, readme FROM mods WHERE path = ?", path).Scan(&version, &t, &readme)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Printf("Module %s not found", path)
+			http.NotFound(w, r)
+			return
+		}
+		log.Printf("Error querying database for module %s: %v", path, err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	log.Printf("Module %s found: version=%s, time=%s", path, version, t.Format(time.RFC3339))
+	modPageData := &ModPageData{
+		Path:    path,
+		Version: version,
+		Readme:  readme.String,
+		Time:    t,
+	}
+	tmpl, err := template.New("mod.html").ParseFiles("templates/mod.html")
+	if err != nil {
+		log.Printf("Error parsing template: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, modPageData)
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
+
+type ModPageData struct {
+	Path    string
+	Version string
+	Readme  string
+	Time    time.Time
 }
